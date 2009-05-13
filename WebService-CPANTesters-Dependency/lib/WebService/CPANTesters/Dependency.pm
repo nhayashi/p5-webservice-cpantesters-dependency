c42080cf7c6b2023799d478336e4234246a3da78
typomaster <naritoshi.hayashi@gmail.com>
2009-05-14 01:43:10 +0900

    create module-setup WebService-CPANTesters-Dependency

---
+++ WebService-CPANTesters-Dependency/lib/WebService/CPANTesters/Dependency.pm
@@ -0,0 +1,33 @@
+package WebService::CPANTesters::Dependency;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WebService::CPANTesters::Dependency -
+
+=head1 SYNOPSIS
+
+  use WebService::CPANTesters::Dependency;
+
+=head1 DESCRIPTION
+
+WebService::CPANTesters::Dependency is
+
+=head1 AUTHOR
+
+typomaster E<lt>naritoshi.hayashi@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
