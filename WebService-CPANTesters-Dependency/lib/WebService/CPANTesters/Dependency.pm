9acf159e34df92058840c770ee059ebc1bdfa7d4
typomaster <naritoshi.hayashi@gmail.com>
2009-05-18 18:41:54 +0900

    create new function.

--- WebService-CPANTesters-Dependency/lib/WebService/CPANTesters/Dependency.pm
+++ WebService-CPANTesters-Dependency/lib/WebService/CPANTesters/Dependency.pm
@@ -2,7 +2,57 @@ package WebService::CPANTesters::Dependency;
 
 use strict;
 use warnings;
+
 our $VERSION = '0.01';
+
+use base qw/Class::Accessor::Fast/;
+
+use Carp qw/croak/;
+use LWP::UserAgent;
+use URI::Template::Restrict;
+use XML::LibXML::XPathContext;
+use XML::LibXML::XPathExpression;
+
+__PACKAGE__->mk_accessors(qw/module depth dependencies/);
+
+sub new {
+    my ($class, %args) = @_;
+    my $tmpl = URI::Template::Restrict->new(
+        q#http://deps.cpantesters.org/?{-join|;|module,perl,os,xml}#);
+    my $uri = $tmpl->process(+{
+        xml => $args->{xml} || 1,
+        module => $args->{module} || croak "need to set module name",
+        perl => $args->{perl} || '5.8.5',
+        os => $args->{os} || 'Linux',
+    });
+    my $ua = LWP::UserAgent->new();
+    my $response = $ua->get($uri);
+    croak "can't get uri: $uri" unless ($response->is_success);
+    my $parser = XML::LibXML->new();
+    my $doc = $parser->parse_string($response->content);
+    my $xpc = XML::LibXML::XPathContext->new($doc);
+    bless { %args, _xpc => $xpc }, $class;
+}
+
+sub module { shift->{module} }
+
+sub find {
+    my $self = shift;
+    my $compiled = XML::LibXML::XPathExpression->new('/cpandeps/dependency');
+    my @nodes = $self->_xpc->find($compiled);
+    $self->{_nodes} = \@nodes;
+    return $self;
+}
+
+sub find_recursive {
+}
+
+sub depth {
+}
+
+sub dependencies {
+    return @{ shift->{_nodes} };
+}
 
 1;
 __END__
---
+++ WebService-CPANTesters-Dependency/t/01_new.t
@@ -0,0 +1,14 @@
+use strict;
+use Test::More tests => 1;
+
+use WebService::CPANTesters::Dependency;
+
+my $deps = WebService::CPANTesters::Dependency->new(+{
+    module => 'Catalyst::Runtime',
+    perl => '5.8.5',
+    os => 'Linux',
+    xml => 1,
+});
+
+isa_ok($deps, 'WebService::CPANTesters::Dependency');
+
